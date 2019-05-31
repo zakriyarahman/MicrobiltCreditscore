1a2a8ad64038c49c043e041b4a66ec39a12752aa
Zak Rahman <zakriyarahman@gmail.com>
2019-05-31 15:40:54 -0500

    Update sample file

--- sample-microbilt.php
+++ sample-microbilt.php
@@ -1,4 +1,14 @@
 <?php
+
+use Zakriyarahman\Microbilt\Map\MicrobiltServiceMap;
+use Zakriyarahman\Microbilt\Map\MicrobiltStructMap;
+use Zakriyarahman\Microbilt\Transform\MicrobiltStructTransform;
+use Zakriyarahman\Microbilt\Transform\MicrobiltServiceTransform;
+use Zakriyarahman\Microbilt\Get\MicrobiltServiceGet;
+use Zakriyarahman\Microbilt\Get\Report\MicrobiltStructGetReport;
+use Zakriyarahman\Microbilt\Get\Report\MicrobiltStructGetRawReport;
+use Zakriyarahman\Microbilt\Get\Report\MicrobiltStructGetArchiveReport;
+
 /**
  * Test with Microbilt for 'https://creditserver.microbilt.com/WebServices/Ex/ExStd.svc?wsdl'
  * @package Microbilt
@@ -16,12 +26,12 @@ require_once dirname(__FILE__) . '/MicrobiltAutoload.php';
 /**
  * Wsdl instanciation infos. By default, nothing has to be set.
  * If you wish to override the SoapClient's options, please refer to the sample below.
- * 
+ *
  * This is an associative array as:
  * - the key must be a MicrobiltWsdlClass constant beginning with WSDL_
  * - the value must be the corresponding key value
  * Each option matches the {@link http://www.php.net/manual/en/soapclient.soapclient.php} options
- * 
+ *
  * Here is below an example of how you can set the array:
  * $wsdl = array();
  * $wsdl[MicrobiltWsdlClass::WSDL_URL] = 'https://creditserver.microbilt.com/WebServices/Ex/ExStd.svc?wsdl';
@@ -30,7 +40,7 @@ require_once dirname(__FILE__) . '/MicrobiltAutoload.php';
  * $wsdl[MicrobiltWsdlClass::WSDL_LOGIN] = 'myLogin';
  * $wsdl[MicrobiltWsdlClass::WSDL_PASSWD] = '**********';
  * etc....
- * Then instantiate the Service class as: 
+ * Then instantiate the Service class as:
  * - $wsdlObject = new MicrobiltWsdlClass($wsdl);
  */
 /**
